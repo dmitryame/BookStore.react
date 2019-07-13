07744b2ae5d075fb4e89babefdf0d0879ddeaad1
dmitryame <dmitry@custommode.com>
2019-07-12 22:23:14 -0400

    add book

--- src/components/BookAdd.jsx
+++ src/components/BookAdd.jsx
@@ -3,7 +3,9 @@ import { connect, } from 'react-redux'
 import { addBook, } from '../actions'
 
 class BookAdd extends React.Component {
-  state = { title: '', content: '', };
+  state = {
+  	title: '', description: '', author: '', tags: '',
+  };
 
   handleChange = event => {
   	this.setState({ [event.target.name]: event.target.value, })
@@ -11,7 +13,15 @@ class BookAdd extends React.Component {
 
   handleSubmit = event => {
   	event.preventDefault()
-  	this.props.addBook(this.state)
+  	const title = this.state.title
+  	const description = this.state.description
+  	const author = this.state.author
+  	const tags = this.state.tags
+  	const book = {
+  		title, description, author, tags,
+  	}
+
+  	this.props.addBook(book)
   };
 
   render() {
@@ -19,19 +29,41 @@ class BookAdd extends React.Component {
 	<div>
   			<h4>Add Book</h4>
 	<form onSubmit={this.handleSubmit}>
-  				<div className="form-group">
-			<input
+			<div className="form-group">
+  					<input
   						type="text" name="title" required value={this.state.title} onChange={this.handleChange}
   						className="form-control" placeholder="Title"
-  					/>
-  </div>
-	<div className="form-group">
-	<textarea
-  						name="content" rows="5" required value={this.state.content} onChange={this.handleChange}
-  						className="form-control" placeholder="Content"
+				/>
+		</div>
+
+
+			<div className="form-group">
+			<label>Author</label>
+			<input
+  						type="text" name="author" required value={this.state.author} onChange={this.handleChange}
+  						className="form-control"
   					/>
   				</div>
-	<button type="submit" className="btn btn-dark">Create</button>
+
+			<div className="form-group">
+	<label>Description</label>
+  					<textarea
+  						name="description" rows="5" required value={this.state.description}
+  						onChange={this.handleChange}
+  						className="form-control"
+				/>
+  				</div>
+
+  				<div className="form-group">
+  					<label>Tags</label>
+  					<input
+  						type="text" name="tags" value={this.state.tags} required onChange={this.handleChange}
+  						className="form-control"
+	/>
+  </div>
+
+
+			<button type="submit" className="btn btn-dark">Create</button>
   			</form>
   		</div>
   	)
